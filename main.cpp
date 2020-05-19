5a22155e9f768c3bcfb688b393d375e1668eb296
Matheus Silva <matffernandesif@gmail.com>
2020-05-19 08:03:22 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
+    cout << "Quantas compras o cliente fez no ultimo ano? " << endl;
+    cout << "Qual o ticket medio? " << endl << endl;
+    cout << "Quantas vezes o cliente atrasou o pagamento ? " << endl;
+    cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B)? " << endl << endl;
+    cout << "Score de volume de compras = " << endl << endl;
+    cout << "Score de inadimplencia = " << endl;
+    cout << "Score de forma de pagamento = " << endl << endl;
+    cout << "Classificacao final = " << endl;
+
     return 0;
 }
